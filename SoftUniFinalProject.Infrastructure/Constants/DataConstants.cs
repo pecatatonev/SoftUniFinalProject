8b6471b0951145ccca76cd07c2ab7971d25a54fa
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-03-08 15:43:04 +0200

    Sponsor Identity

---
+++ SoftUniFinalProject.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniFinalProject.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public static class Sponsor 
+        {
+            public const int NameMaxLenght = 150;
+            public const int NameMinLenght = 3;
+
+            public const int NetWorthMax = 10;
+            public const int NetWorthMin = 1;
+        }
+    }
+}
---
+++ SoftUniFinalProject.Infrastructure/Data/Models/Sponsor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static SoftUniFinalProject.Infrastructure.Constants.DataConstants.Sponsor;
+
+namespace SoftUniFinalProject.Infrastructure.Data.Models
+{
+    public class Sponsor
+    {
+        [Key]
+        [Comment("Sponsor Identifier")]
+        public int Id { get; set; }
+        [Comment("Sponsor Name")]
+        [StringLength(NameMaxLenght)]
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Comment("Sponsor NetWorth")]
+        [Range(NetWorthMin, NetWorthMax)]
+        [Required]
+        public int NetWorthInBillion { get; set; }
+        [Comment("Date of creation")]
+        [Required]
+        public DateTime StartOn { get; set; }
+        [Comment("Logo of the sponsor")]
+        public string ImageUrl { get; set; } = string.Empty;
+
+    }
+}
