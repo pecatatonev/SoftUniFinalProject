f930ab2ea27d1e03ba83766ead24c148a7ca44a0
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-03-08 17:15:43 +0200

    Football Game Entity Created

--- SoftUniFinalProject.Infrastructure/Data/Models/FootballGame.cs
+++ SoftUniFinalProject.Infrastructure/Data/Models/FootballGame.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static SoftUniFinalProject.Infrastructure.Constants.DataConstants.FootballGame;
 
 namespace SoftUniFinalProject.Infrastructure.Data.Models
@@ -13,17 +8,22 @@ namespace SoftUniFinalProject.Infrastructure.Data.Models
     public class FootballGame
     {
         [Key]
+        [Comment("Football Game Identifier")]
         public int Id { get; set; }
         [Required]
         [Comment("Reason for the game")]
         public string PlayingFor { get; set; } = string.Empty;
         [Required]
+        [Comment("Football Game Home Team Identifier")]
         public int HomeTeamId { get; set; }
         [ForeignKey(nameof(HomeTeamId))]
+        [Comment("Football Game Home Team")]
         public Team HomeTeam { get; set; } = null!;
         [Required]
+        [Comment("Football Game Away Team Identifier")]
         public int AwayTeamId { get; set; }
         [ForeignKey(nameof(AwayTeamId))]
+        [Comment("Football Game Away Team")]
         public Team AwayTeam { get; set; } = null!;
         [Required]
         [Comment("Referee of the match name")]
