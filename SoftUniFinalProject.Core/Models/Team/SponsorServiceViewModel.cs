6c8284107ed959d88e28efa8fa71af870520abc8
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-03-13 21:49:49 +0200

    Making sponsor view and controller

--- SoftUniFinalProject.Core/Models/Team/SponsorServiceViewModel.cs
+++ SoftUniFinalProject.Core/Models/Team/SponsorServiceViewModel.cs
@@ -5,15 +5,9 @@ namespace SoftUniFinalProject.Core.Models.Team
 {
     public class SponsorServiceViewModel
     {
-        [StringLength(NameMaxLenght, MinimumLength = NameMinLenght, ErrorMessage = NameLenght)]
-        [Required]
         public string Name { get; set; } = string.Empty;
-        [Range(NetWorthMin, NetWorthMax, ErrorMessage = NetWorthRange)]
-        [Required]
         public int NetWorthInBillion { get; set; }
-        [Required]
         public string StartOn { get; set; } = string.Empty;
-        [Required]
         public string ImageUrl { get; set; } = string.Empty;
     }
 }
