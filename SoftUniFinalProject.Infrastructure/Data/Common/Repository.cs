5c08d2a0523795ae455e548ea68a784b0674e40e
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-03-13 20:32:03 +0200

    Repository for db created

---
+++ SoftUniFinalProject.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniFinalProject.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+        private readonly DbContext context;
+        public Repository(FootballEventDbContext _context)
+        {
+            context = _context;
+        }
+
+        //Returning table of T
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return context.Set<T>();
+        }
+
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>().AsNoTracking();
+        }
+    }
+}
