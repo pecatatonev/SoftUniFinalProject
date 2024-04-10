193aab375c0e87d4c7a249be7ab4f7aea9dbf3af
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-04-10 13:11:33 +0300

    Hiding password for email

--- SoftUniFinalProject.Core/Services/HomeService/HomeService.cs
+++ SoftUniFinalProject.Core/Services/HomeService/HomeService.cs
@@ -7,17 +7,31 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Configuration;
+using System.Collections;
 
 namespace SoftUniFinalProject.Core.Services.HomeService
 {
     public class HomeService : IHomeService
     {
+        private readonly IConfiguration _configuration;
+
+        public HomeService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetPassword()
+        {
+            return Environment.GetEnvironmentVariable("MyEmailPass");
+        }
         public async Task SendEmailAsync(string email, string subject, string message)
         {
             try
             {
+
+                string secretPass = GetPassword();
                 var mail = "petpuppshop@gmail.com";
-                var pass = "igxw kjdk dzwj jgnp";
                 var mailTo = "forworkemail032024@gmail.com";
 
                 var client = new SmtpClient("smtp.gmail.com", 587)
@@ -26,7 +40,7 @@ namespace SoftUniFinalProject.Core.Services.HomeService
                 };
 
                 client.UseDefaultCredentials = false;
-                client.Credentials = new NetworkCredential(mail, pass);
+                client.Credentials = new NetworkCredential(mail, secretPass);
 
                 await client.SendMailAsync(new MailMessage(from: email,
                     to: mailTo,
