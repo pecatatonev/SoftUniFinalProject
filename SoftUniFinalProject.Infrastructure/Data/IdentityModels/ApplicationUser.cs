023fc5b2c395b965883359d4dd08896910f151f0
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-04-12 16:57:23 +0300

    Application User added

---
+++ SoftUniFinalProject.Infrastructure/Data/IdentityModels/ApplicationUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SoftUniFinalProject.Infrastructure.Constants.DataConstants.ApplicationUser;
+
+namespace SoftUniFinalProject.Infrastructure.Data.IdentityModels
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [MaxLength(FirstNameMaxLenght)]
+        public string? FirstName { get; set; }
+        [MaxLength(LastNameMaxLenght)]
+        public string? LastName { get; set;}
+    }
+}
