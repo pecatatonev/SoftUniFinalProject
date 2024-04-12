f3b5e828612da9df2866419ac203236a985e4ec8
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-04-12 20:15:23 +0300

    Making ViewModels For Roles

---
+++ SoftUniFinalProject.Core/Models/Admin/UserToRoleAddViewModel.cs
@@ -0,0 +1,21 @@
+﻿using SoftUniFinalProject.Infrastructure.Constants;
+using SoftUniFinalProject.Infrastructure.Data.IdentityModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniFinalProject.Core.Models.Admin
+{
+    public class UserToRoleAddViewModel
+    {
+        [Required(ErrorMessage = DataConstants.RequiredMessage)]
+        public string UserId { get; set; } = string.Empty;
+        public ICollection<UsersViewModel> Users { get; set; } = new List<UsersViewModel>();
+        [Required(ErrorMessage = DataConstants.RequiredMessage)]
+        public string RoleId { get; set; } = string.Empty;
+        public ICollection<RoleListViewModel> Roles { get; set; } = new List<RoleListViewModel>();
+    }
+}
