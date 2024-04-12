925aa701a8c4edb90936d399f6a380ef96649496
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-04-12 20:14:48 +0300

    Making UserService

---
+++ SoftUniFinalProject.Core/Services/Admin/UserService/UserService.cs
@@ -0,0 +1,25 @@
+﻿using SoftUniFinalProject.Core.Contracts.Admin.Identity;
+using SoftUniFinalProject.Core.Models.Admin;
+using SoftUniFinalProject.Infrastructure.Data.IdentityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniFinalProject.Core.Services.Admin.UserService
+{
+    public class UserService : IUserService
+    {
+        public ApplicationRole RoleCreate(string roleName)
+        {
+            var role = new ApplicationRole()
+            {
+                Name = roleName,
+                NormalizedName = roleName.ToUpper()
+            };
+
+            return role;
+        }
+    }
+}
