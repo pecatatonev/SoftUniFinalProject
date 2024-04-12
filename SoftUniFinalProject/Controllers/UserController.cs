a7a7f76a696c83e45b76b01f7845616af74ea733
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-04-12 16:59:28 +0300

    Implementing UserController

---
+++ SoftUniFinalProject/Controllers/UserController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SoftUniFinalProject.Core.Models.Identity;
+using SoftUniFinalProject.Infrastructure.Data.IdentityModels;
+
+namespace SoftUniFinalProject.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly RoleManager<ApplicationRole> roleManager;
+
+        private readonly UserManager<ApplicationUser> userManager;
+
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public UserController(
+            UserManager<ApplicationUser> _userManager,
+            SignInManager<ApplicationUser> _signInManager,
+            RoleManager<ApplicationRole> _roleManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+            roleManager = _roleManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new ApplicationUser()
+            {
+                Email = model.Email,
+                UserName = model.UserName,
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Login", "User");
+            }
+
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new LoginViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await userManager.FindByNameAsync(model.UserName);
+
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid login");
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [AllowAnonymous]
+        public async Task<IActionResult> AddRole(string roleName)
+        {
+            if (await roleManager.RoleExistsAsync(roleName) == false)
+            {
+                var role = new ApplicationRole()
+                {
+                    Name = roleName,
+                    NormalizedName = roleName.ToUpper()
+                };
+
+                await roleManager.CreateAsync(role);
+            }
+            
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
