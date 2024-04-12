0046cdbd17447613f273e76fffe2cd1bceb3d023
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-04-12 16:55:22 +0300

    Login and Register View Models Added

---
+++ SoftUniFinalProject.Core/Models/Identity/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using SoftUniFinalProject.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniFinalProject.Core.Models.Identity
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = DataConstants.RequiredMessage)]
+        public string UserName { get; set; } = null!;
+
+        [Required(ErrorMessage = DataConstants.RequiredMessage)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ SoftUniFinalProject.Core/Models/Identity/RegisterViewModel.cs
@@ -0,0 +1,40 @@
+﻿using SoftUniFinalProject.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SoftUniFinalProject.Infrastructure.Constants.DataConstants.ApplicationUser;
+
+namespace SoftUniFinalProject.Core.Models.Identity
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = DataConstants.RequiredMessage)]
+        [StringLength(FirstNameMaxLenght, MinimumLength = FirstNameMinLenght)]
+        public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = DataConstants.RequiredMessage)]
+        [StringLength(LastNameMaxLenght, MinimumLength = LastNameMinLenght)]
+        public string LastName { get; set; } = null!;
+
+        [Required(ErrorMessage = DataConstants.RequiredMessage)]
+        [StringLength(UserNameMaxLenght, MinimumLength = UserNameMinLenght)]
+        public string UserName { get; set; } = null!;
+
+        [Required(ErrorMessage = DataConstants.RequiredMessage)]
+        [EmailAddress]
+        [StringLength(EmailMaxLenght, MinimumLength = EmailMinLenght)]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = DataConstants.RequiredMessage)]
+        [StringLength(PasswordMaxLenght, MinimumLength = PasswordMinLenght)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
