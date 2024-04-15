6925e029ea24298787f432fc25af50b9a20dd6e7
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-04-16 01:58:47 +0300

    Creating view model for creating football game

---
+++ SoftUniFinalProject.Core/Models/Team/AddTeamToFootballGameViewModel.cs
@@ -0,0 +1,16 @@
+﻿using SoftUniFinalProject.Infrastructure.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftUniFinalProject.Core.Models.Team
+{
+    public class AddTeamToFootballGameViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
