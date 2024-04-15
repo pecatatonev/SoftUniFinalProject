a961f364f922b260064577167b4d1609b119f5ff
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-04-15 20:36:35 +0300

    Creating Admin Area

---
+++ SoftUniFinalProject/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static SoftUniFinalProject.Core.Constants.RoleConstants;
+
+namespace SoftUniFinalProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = AdministratorRole)]
+    public class HomeController : Controller
+    {
+        public IActionResult Users()
+        {
+            return View();
+        }
+    }
+}
