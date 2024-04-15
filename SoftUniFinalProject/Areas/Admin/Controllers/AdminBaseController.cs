edeee3e2a40471bcd831ef2403870c1f337ee508
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-04-15 23:44:44 +0300

    Creating admin Base Controller

---
+++ SoftUniFinalProject/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using static SoftUniFinalProject.Core.Constants.RoleConstants;
+
+namespace SoftUniFinalProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = AdministratorRole)]
+    public class AdminBaseController : Controller
+    {
+    }
+}
