4fb92fb9656b26af08ad9ec4f25e2fcce315bf02
pecatatonev <114527717+pecatatonev@users.noreply.github.com>
2024-04-17 16:18:42 +0300

    User Service Tests Created

---
+++ SoftUniFinalProject.UnitTests/UserServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUniFinalProject.Core.Services.AttendanceService;
+using SoftUniFinalProject.Infrastructure.Data.Common;
+using SoftUniFinalProject.Infrastructure.Data.Models;
+using SoftUniFinalProject.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SoftUniFinalProject.Core.Services.Admin.UserService;
+
+namespace SoftUniFinalProject.UnitTests
+{
+    public class UserServiceTests
+    {
+        private UserService userService;
+
+        [SetUp]
+        public void Setup()
+        {
+           userService = new UserService();
+        }
+
+        [Test]
+        public void RoleCreate_ReturnsCorrectRole()
+        {
+            // Arrange
+            string roleName = "TestRole";
+
+            // Act
+            var role = userService.RoleCreate(roleName);
+
+            // Assert
+            Assert.NotNull(role);
+            Assert.AreEqual(roleName, role.Name);
+            Assert.AreEqual(roleName.ToUpper(), role.NormalizedName);
+        }
+    }
+}
